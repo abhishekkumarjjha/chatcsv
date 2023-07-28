e98c8b966a07d236c4a569330ee03a4969957619
abhishekkumarjjha <127896599+abhishekkumarjjha@users.noreply.github.com>
2023-07-28 17:44:06 -0500

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,4 +1,3 @@
-
 import langchain
 from langchain.llms import OpenAI
 from langchain.agents import create_pandas_dataframe_agent
@@ -8,64 +7,25 @@ API_KEY = "[YOUR-OPENAI-KEY]"
 
 
 def create_agent(filename: str):
-    """
-    Create an agent that can access and use a large language model (LLM).
-
-    Args:
-        filename: The path to the CSV file that contains the data.
-
-    Returns:
-        An agent that can access and use the LLM.
-    """
-
-    # Create an OpenAI object.
     llm = OpenAI(openai_api_key=API_KEY)
-
-    # Read the CSV file into a Pandas DataFrame.
     df = pd.read_csv(filename)
-
-    # Create a Pandas DataFrame agent.
     return create_pandas_dataframe_agent(llm, df, verbose=False)
 
-
 def query_agent(agent, query):
-    """
-    Query an agent and return the response as a string.
-
-    Args:
-        agent: The agent to query.
-        query: The query to ask the agent.
-
-    Returns:
-        The response from the agent as a string.
-    """
-
     prompt = (
         """
-            If it is just asking a question, reply as follows:
-            {"answer": "answer"}
-            Example:
-            {"answer": "The title with the highest rating is 'Gilead'"}
+        If it is just asking a question that requires an answer, reply as follows:
+        {"answer": "answer"}
+        Example:
+        {"answer": "The title with the highest rating is 'Gilead'"}
 
-            If you do not know the answer, reply as follows:
-            {"answer": "I do not know."}
+        If you do not know the answer, reply as follows:
+        {"answer": "I do not know."}
 
-            Return all output as a string.
-
-            All strings in "columns" list and data list, should be in double quotes,
-
-            For example: {"columns": ["title", "ratings_count"], "data": [["Gilead", 361], ["Spider's Web", 5164]]}
-
-            Lets think step by step.
-
-            Below is the query.
-            Query: 
-            """
+        Return all output as a string.
+        """
         + query
     )
 
-    # Run the prompt through the agent.
     response = agent.run(prompt)
-
-    # Convert the response to a string.
     return response.__str__()
